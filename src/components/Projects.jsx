48ce0c7c4aafe44266f2c5232569153dab889cdf
Sohamb2707 <122520652+Sohamb2707@users.noreply.github.com>
2024-12-27 17:56:37 +0530

    Add files via upload

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const Projects = () => {
       >
         <ProjectCard
           src={SwiftFix}
-          codeLink="https://github.com/Ade-mir/company-landing-page-2"
+          codeLink="https://github.com/Sohamb2707/SwiftFix.git"
           demoLink="https://drive.google.com/file/d/1MW1OLElwDA9hN_SbxBY0G1trUaLxqYuO/view?usp=drive_link"
           h3="SwiftFix"
           p="A home service app offering a wide range of services including plumbing, cleaning, and electrical repairs. Built using the MERN stack with a focus on user-friendly design and efficient service delivery."
@@ -46,7 +46,7 @@ const Projects = () => {
         />
         <ProjectCard
           src={CareMate}
-          codeLink="https://github.com/Ade-mir/company-landing-page-2"
+          codeLink="https://github.com/Sohamb2707/CareMate.git"
           demoLink="https://drive.google.com/file/d/1dyrio-j_9pI-iEVD-lXF2H2pyRGMkR2n/view?usp=drive_link"
           h3="CareMate"
           p="A mobile app that connects elderly individuals with caregivers. Features include real-time communication, personalized care plans, and a robust scheduling system."
@@ -54,7 +54,7 @@ const Projects = () => {
         />
         <ProjectCard
           src={Tomato}
-          codeLink="https://github.com/Ade-mir/company-landing-page-2"
+          codeLink="https://github.com/Sohamb2707/Tomatoo-Food-Delivery-.git"
           demoLink="https://drive.google.com/file/d/1Uwkg2yB0ouDa3pJczChl9ecrgXA5xMPg/view?usp=drive_link"
           h3="Tomato"
           p="A responsive food delivery website where key features include adding items to the menu, removing from the cart, dynamically calculating the total price in 
